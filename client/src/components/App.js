ce569f7c713b4d0c3c23f2af7798c2c08e5a6646
ryand67 <rwdevenney@gmail.com>
2020-09-30 15:39:30 -0400

    button styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Editor from './Editor';
 
 function App() {
   const [html, setHtml] = useState('');
   const [css, setCss] = useState('');
   const [js, setJs] = useState('');
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>
+      `)
+    }, 450)
+
+    return () => clearTimeout(timeout);
+  }, [html, js, css])
+
   return (
     <>
       <div className="pane top-pane">
@@ -27,6 +43,7 @@ function App() {
       </div>
       <div className="pane">
         <iframe 
+          srcDoc={srcDoc}
           title="output"
           sandbow="allow-scripts"
           frameBorder="0"
